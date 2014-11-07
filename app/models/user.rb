aa7861c6eee8380cc7c63fc24b0f5548cd9bea68
Orlando Bucao <orlando.bucao@biola.edu>
2014-11-07 21:35:18 +0000

    Add user model, controller and view

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User
+  include Mongoid::Document
+
+  field :username, type: String
+  field :first_name, type: String
+  field :last_name, type: String
+  field :email, type: String
+  field :photo_url, type: String
+  field :biola_id, type: String
+  field :affiliations, type: Array
+  field :entitlements, type: Array
+  
+  def name
+    "#{first_name} #{last_name}"
+  end
+end
